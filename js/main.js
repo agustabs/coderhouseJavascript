0845b8e28ce5a31452305f3278e6151a84dafdbd
Agustin Taborda <ataborda@Lenovo.com>
2022-04-27 02:22:32 -0300

    v1.2

--- js/main.js
+++ js/main.js
@@ -25,20 +25,27 @@ function cardsCreator() {
   cleanCards();
   watchlist.forEach((movie) => {
     const movieCard = document.createElement("div");
-    movieCard.classList.add("card");
+    movieCard.classList.add("card", "movieCard", "m-3", "col-4");
 
-    const movieName = document.createElement("h3");
+    const movieName = document.createElement("div");
     movieName.textContent = movie.name;
+    movieName.classList.add("card-header", "fw-bold");
 
-    const movieGender = document.createElement("p");
+    const movieUl = document.createElement("ul");
+    movieUl.classList.add("list-group", "list-group-flush");
+
+    const movieGender = document.createElement("li");
     movieGender.textContent = movie.gender;
+    movieGender.classList.add("list-group-item");
 
-    const movieRate = document.createElement("b");
+    const movieRate = document.createElement("li");
     movieRate.textContent = movie.rate;
+    movieRate.classList.add("list-group-item", "fw-bold");
 
     movieCard.appendChild(movieName);
-    movieCard.appendChild(movieGender);
-    movieCard.appendChild(movieRate);
+    movieCard.appendChild(movieUl);
+    movieUl.appendChild(movieGender);
+    movieUl.appendChild(movieRate);
     movieCards.appendChild(movieCard);
   });
 }
