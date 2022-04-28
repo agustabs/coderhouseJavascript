a53d4baba938e87c94588df161a2a60c5d69f1bb
Agustin Taborda <ataborda@Lenovo.com>
2022-04-28 02:06:49 -0300

    v2.0

---
+++ js/function.js
@@ -0,0 +1,108 @@
+//funciones
+function cardsCreator(movies) {
+  cleanCards();
+  movies.forEach((movie) => {
+    const movieCard = document.createElement("div");
+    movieCard.classList.add("card", "movieCard", "m-3", "col-4");
+
+    const movieName = document.createElement("div");
+    movieName.textContent = movie.tittle;
+    movieName.classList.add("card-header", "fw-bold");
+
+    const movieUl = document.createElement("ul");
+    movieUl.classList.add("list-group", "list-group-flush");
+
+    const movieGender = document.createElement("li");
+    movieGender.textContent = movie.gender;
+    movieGender.classList.add("list-group-item");
+
+    const movieRate = document.createElement("li");
+    movieRate.textContent = movie.rate;
+    movieRate.classList.add("list-group-item", "fw-bold");
+
+    const linkTrailer = document.createElement("a");
+    linkTrailer.textContent = "see the trailer";
+    linkTrailer.setAttribute("href", movie.trailer);
+    linkTrailer.setAttribute("target", "_blank");
+    linkTrailer.classList.add("btn", "btn-warning");
+
+    const deleteMovieBtn = document.createElement("a");
+    deleteMovieBtn.textContent = "delete movie";
+    deleteMovieBtn.classList.add("btn", "btn-danger", "m-2");
+    deleteMovieBtn.addEventListener("click", () => {
+      deleteMovie(movie);
+    });
+
+    movieCard.appendChild(movieName);
+    movieCard.appendChild(movieUl);
+    movieUl.appendChild(movieGender);
+    movieUl.appendChild(movieRate);
+    movieUl.appendChild(linkTrailer);
+    movieUl.appendChild(deleteMovieBtn);
+    movieCards.appendChild(movieCard);
+  });
+}
+//cardsCleaner
+function cleanCards() {
+  const movieCards = document.getElementById("movieCards");
+  movieCards.innerHTML = "";
+}
+//handler user form
+function handlerUserForm(e) {
+  e.preventDefault();
+  userName = document.getElementById("user").value;
+
+  let movieCards = document.getElementById("movieCards");
+  const films = JSON.parse(localStorage.getItem(userName));
+
+  if (films == null) {
+    movieCards.innerHTML = "<h1>it looks empty here, try adding a movie</h1>";
+  } else {
+    cardsCreator(films);
+  }
+  welcomeUser();
+  e.target.reset();
+}
+//welcome to the user
+function welcomeUser() {
+  const welcomeBanner = document.getElementById("welcomeBanner");
+
+  welcomeBanner.innerHTML = `<h2>hello, ${userName}</h2>
+    <form id="movieForm" class="md-form mb-5">
+      <input type="text" id="tittle" placeholder="tittle" class="form-control validate">
+      <input type="number" id="rate" placeholder="rate" class="form-control validate">
+      <input type="text" id="gender" placeholder="gender" class="form-control validate">
+      <input type="text" id="trailer" placeholder="trailer's url" class="form-control validate">
+      <button type="submit" class="btn btn-warning">add movie</button>
+    </form>`;
+
+  document.getElementById("movieForm").addEventListener("submit", addMovie);
+}
+//add a movie
+function addMovie(e) {
+  e.preventDefault();
+  const tittle = document.getElementById("tittle").value;
+  const rate = document.getElementById("rate").value;
+  const gender = document.getElementById("gender").value;
+  const trailer = document.getElementById("trailer").value;
+
+  const movie = new movies(tittle, rate, gender, trailer);
+
+  const moviesOnLocalStorage = JSON.parse(localStorage.getItem(userName));
+  if (moviesOnLocalStorage == null) {
+    localStorage.setItem(userName, JSON.stringify([movie]));
+  } else {
+    moviesOnLocalStorage.push(movie);
+    localStorage.setItem(userName, JSON.stringify(moviesOnLocalStorage));
+    cardsCreator(moviesOnLocalStorage);
+  }
+  e.target.reset();
+}
+function deleteMovie(movie) {
+  const moviesOnLocalStorage = JSON.parse(localStorage.getItem(userName));
+  const newArray = moviesOnLocalStorage.filter(
+    (item) => item.tittle != movie.tittle
+  );
+  localStorage.setItem(userName, JSON.stringify(newArray));
+  cardsCreator(newArray);
+}
--- js/main.js
+++ js/main.js
@@ -1,76 +1,12 @@
 //constructor
 class movies {
-  constructor(name, rate, gender) {
-    this.name = name.toLowerCase();
+  constructor(tittle, rate, gender, trailer) {
+    this.tittle = tittle.toLowerCase();
     this.rate = parseFloat(rate);
     this.gender = gender.toLowerCase();
+    this.trailer = trailer;
   }
 }
-//database
-const watchlist = [
-  { name: "the godfather", rate: 8.5, gender: "drama" },
-  { name: "titanic", rate: 8, gender: "drama", rate: 8 },
-  { name: "interstellar", rate: 8, gender: "sci-fi", rate: 8 },
-  { name: "the dark knight", rate: 9, gender: "action", rate: 9 },
-  { name: "amelie", rate: 7.5, gender: "comedy", rate: 7.5 },
-];
-
-//cards creator
-const movieCards = document.getElementById("movieCards");
-document.addEventListener("DOMContentLoaded", () => {
-  cardsCreator();
-});
-
-function cardsCreator() {
-  cleanCards();
-  watchlist.forEach((movie) => {
-    const movieCard = document.createElement("div");
-    movieCard.classList.add("card", "movieCard", "m-3", "col-4");
-
-    const movieName = document.createElement("div");
-    movieName.textContent = movie.name;
-    movieName.classList.add("card-header", "fw-bold");
-
-    const movieUl = document.createElement("ul");
-    movieUl.classList.add("list-group", "list-group-flush");
-
-    const movieGender = document.createElement("li");
-    movieGender.textContent = movie.gender;
-    movieGender.classList.add("list-group-item");
-
-    const movieRate = document.createElement("li");
-    movieRate.textContent = movie.rate;
-    movieRate.classList.add("list-group-item", "fw-bold");
-
-    movieCard.appendChild(movieName);
-    movieCard.appendChild(movieUl);
-    movieUl.appendChild(movieGender);
-    movieUl.appendChild(movieRate);
-    movieCards.appendChild(movieCard);
-  });
-}
-
-//add button event
-const btnAdd = document.getElementById("btnAdd");
-btnAdd.onclick = () => {
-  let addMovie = prompt(
-    "To enter a movie yo your watchlist type 'add': "
-  ).toLowerCase();
-
-  while (addMovie == "add") {
-    watchlist.push(
-      new movies(
-        prompt("Enter film tittle").toLowerCase(),
-        parseFloat(prompt("Type rate")),
-        prompt("Gender: ").toLowerCase()
-      )
-    );
-    addMovie = prompt("Want to add another movie? add/esc").toLowerCase();
-  }
-  cardsCreator();
-};
-
-//cardsCleaner
-function cleanCards() {
-  movieCards.innerHTML = "";
-}
+let userName = document
+  .getElementById("userForm")
+  .addEventListener("submit", handlerUserForm);
